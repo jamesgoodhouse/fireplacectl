d6a2aafff4c76bbcced4fff89af2048d2115ed83
James J. Goodhouse <j.goodhouse@gmail.com>
2021-03-10 20:16:21 -0800

    make some tweaks

--- fireplacectl.py
+++ fireplacectl.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
 
-# A simple Python application for controlling a relay board from a Raspberry Pi
-# The application uses the GPIO Zero library (https://gpiozero.readthedocs.io/en/stable/)
-# The relay is connected to one of the Pi's GPIO ports, then is defined as an Output device
-# in GPIO Zero: https://gpiozero.readthedocs.io/en/stable/api_output.html#outputdevice
-
+import os
 import signal
 import sys
-import os
-from time import sleep
 
 import gpiozero
 import paho.mqtt.client as mqtt
@@ -31,50 +25,52 @@ mqttc = mqtt.Client()
 
 def signal_handler(signal, frame):
     print("\nshutting down\n")
-    fireplace_off()
-    fireplace.close()
-    sleep(5)
+    mqttc.loop_stop()
+
+    # some grossness to ensure publishes go through. can't use
+    # wait_for_publish() while loop_forever() is still going.
+    mqttc.loop_start()
+    fireplace_off().wait_for_publish()
+    print("setting status to 'OFFLINE'")
+    set_status("OFFLINE").wait_for_publish()
+    mqttc.loop_stop()
+
     mqttc.disconnect()
+
     sys.exit(0)
+
+def set_status(status):
+    return mqttc.publish(topic=TOPIC__STATUS, payload=status, qos=2, retain=True)
 
 def mqtt_on_connect(client, userdata, flags, rc):
     print("connected with result code "+str(rc))
+    print("setting status to 'ONLINE'")
+    set_status("ONLINE")
     print("subscribing to 'rpi/fireplace/power' topic")
-    client.publish(topic=TOPIC__STATUS, payload="ON", qos=2, retain=True)
-    client.subscribe("rpi/fireplace/power")
+    client.subscribe(TOPIC__POWER)
 
-def mqtt_on_disconnect(client, userdata, rc):
-    print("disconnected")
-
-def mqtt_on_publish(client, userdata, result):
-    print("data published")
-
-# convert to message_callback_add()
-def mqtt_on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
-    if msg.topic == TOPIC__POWER:
-        payload = msg.payload.decode("utf-8")
-        if payload == "ON":
-            print("received 'ON' command")
-            fireplace_on()
-        elif payload == "OFF":
-            print("received 'OFF' command")
-            fireplace_off()
-        else:
-            print("ignoring unknown payload '"+payload+"'")
+def power_topic(client, userdata, msg):
+    payload = msg.payload.decode()
+    if payload == "ON":
+        print("received 'ON' command")
+        fireplace_on()
+    elif payload == "OFF":
+        print("received 'OFF' command")
+        fireplace_off()
+    else:
+        print("ignoring unknown payload '"+payload+"'")
 
 def fireplace_on():
     print("turning on fireplace")
     fireplace.on()
     print("publishing 'ON' state")
-    mqttc.publish(topic=TOPIC__STATE, payload="ON", qos=2, retain=True)
+    return mqttc.publish(topic=TOPIC__STATE, payload="ON", qos=2, retain=True)
 
 def fireplace_off():
     print("turning off fireplace")
     fireplace.off()
     print("publishing 'OFF' state")
-    mqttc.publish(topic=TOPIC__STATE, payload="OFF", qos=2, retain=True)
+    return mqttc.publish(topic=TOPIC__STATE, payload="OFF", qos=2, retain=True)
 
 def main_loop():
     # setup signal catching
@@ -84,9 +80,7 @@ def main_loop():
     mqttc.username_pw_set(username=MQTT__USERNAME,password=MQTT__PASSWORD)
     mqttc.connect(MQTT__HOST, MQTT__PORT, 60)
     mqttc.on_connect = mqtt_on_connect
-    mqttc.on_disconnect = mqtt_on_disconnect
-    mqttc.on_message = mqtt_on_message
-    mqttc.on_publish = mqtt_on_publish
+    mqttc.message_callback_add(TOPIC__POWER, power_topic)
 
     fireplace_off()
 
